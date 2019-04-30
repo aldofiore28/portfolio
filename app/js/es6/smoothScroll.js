7de410afd5665c23f2259d81aa183149dfa5e490
aldofiore28 <aldo.fiore95@gmail.com>
2019-04-30 13:01:59 +0100

    remove smooth scroll script

--- app/js/es6/smoothScroll.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.nav-link').forEach(item => {
-    item.addEventListener('click', function (event) {
-        event.preventDefault()
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        })
-    })
-})
